1cc4790c8fff5691dcb2e3f38b1bf9ffb1994afb
Bruno Blauzius Schuindt <bruno_newstudio@hotmail.com>
2016-02-12 18:06:24 -0200

    relatorio

--- View/Reservas/relatorio.php
+++ View/Reservas/relatorio.php
@@ -9,25 +9,22 @@
             <?php endif; ?>
         </div>
         <div style="width: 500px; float: left; margin: 10px;">
-            Nome Fantasia:<br>
-            <strong class="text text-warning"> <?= $empresaJuridica['nome_fantasia'] ?> </strong><br>
-            Data Reservas:<br>
-            <strong class="text text-warning"> <?= $dataReserva ?> </strong><br>
+            Nome Fantasia: <strong class="text text-warning" style="font-size:16px;"> <?= $empresaJuridica['nome_fantasia'] ?> </strong> 
+            Dia: <strong class="text text-warning" style="font-size:16px;"> <?= $dataReserva ?> </strong><br>
         </div>
     </div>
     <hr>
     <div class="clearfix"></div>
 
-        <table class="table table-condensed table-striped">
+        <table class="table table-condensed table-striped" style="font-size:18px;">
             <thead>
                 <tr>
-                    <th style="width:12%" class="text-center">Nome</th>
-                    <th style="width:4%" class="text-center">Horário</th>
-                    <th style="width:10%" class="text-center">Ambiente</th>
+                    <th style="width:15%" class="">Nome</th>
                     <th style="width:3%" class="text-center">PAX</th>
-                    <th style="width:8%" class="text-center">Mesas</th>
-                    <!--th style="width:25%" class="text-center">Observações</th-->
-                    <th style="width:10%" class="text-center">Responsável</th>
+                    <th style="width:4%" class="text-center">Horário</th>
+                    <th style="width:10%" class="text-center">Ambiente/Mesas</th>
+                    <th style="width:15%" class="text-center">Observações</th>
+                    <!--th style="width:10%" class="text-center">Responsável</th-->
                     <!--th style="width:10%" class="text-center">Envio E-mail</th-->
                 </tr>
             </thead>
@@ -37,24 +34,26 @@
                 foreach ($registros as $registro):
             ?>
                     <tr>
-                        <td class="text-center">
+                        <td class="">
                             <strong><?= $registro['cliente'] ?></strong><br>
                             <?= Utils::formatarTelefone($registro['telefone']) ?><br>
                             <?//= $registro['email'] ?>
                         </td>
-                        <td class="text-center"><strong><?= Utils::returnHours( $registro['start'] );?></strong></td>
-                        <td class="text-center"> <strong><?= $registro['ambiente'] ?></strong> </td>
                         <td class="text-center"><strong><?= $registro['qtde_pessoas'] ?></strong></td>
-                        <td  class="text-center"><?= $registro['mesas'] ?></td>
+                        <td class="text-center"><strong><?= Utils::returnHours( $registro['start'] );?></strong></td>
+                        <td class="text-center"> 
+                            <strong><?= $registro['ambiente'] ?> </strong><br>
+                            <?= $registro['mesas'] ?>
+                        </td>
                         <!--td class="text-center">
                             <?//= ($registro['descricao_interna']) ? $registro['descricao_interna'] : 'não informado';?>
                         </td-->
-                        <td class="text-center"><?= $registro['funcionario'] ?></td>
+                        <td class="text-center"><?= (trim($registro['descricao_cliente'])) ? trim($registro['descricao_cliente']) : 'não informado';?></td>
                         <!--td class="text-center"><?//= ($registro['data_envio']) ? Utils::convertData($registro['data_envio']) : '<strong class="text text-danger">E-mail ainda não foi enviado</strong>'; ?></td-->
                     </tr>
-                    <tr>
+                    <!--tr>
                         <td colspan="10">
-                            <strong>Observações:</strong> <?= (trim($registro['descricao_cliente'])) ? trim($registro['descricao_cliente']) : 'não informado';?>
+                            <strong>Observações:</strong> 
                             <?php
 //                                $i = 1;
 //                                foreach ($registro['lista_convidados'] as $convidado):
@@ -66,8 +65,8 @@
 //                            $i++;
 //                                endforeach;
                             ?>
-                        </td>
-                    </tr>
+                        <!--/td>
+                    </tr-->
             <?php
                 endforeach;
             endif;
