844df3a3c75c5beb8a84e2327d2f1c5f47aaca98
Bruno Blauzius Schuindt <bruno_newstudio@hotmail.com>
2016-02-26 16:50:54 -0300

    site

--- View/Pages/index.php
+++ View/Pages/index.php
@@ -1,9 +1,9 @@
-<div id="anchor-home" class="pi-section-w pi-section-parallax pi-section-high piSectionHigh" style="background-image: url(img_external/gallery/laptop-table.jpg);">
+<div id="anchor-home" class="pi-section-w pi-section-parallax pi-section-high piSectionHigh" style="background-image: url(<?= Router::url('View/webroot/site/img/crbst_bg0.png') ?>);">
     <div class="pi-texture" style="background: rgba(24, 28, 32, 0.7);"></div>
     <div class="pi-section pi-text-center">
 
         <h2 class="h1 pi-text-shadow pi-weight-700 pi-uppercase" style="font-size: 60px;">
-            My<span class="pi-text-base">Nyght</span> Reservas
+            My<span class="pi-text-pink">Nyght</span>
         </h2>
 
         <p class="lead-20 pi-weight-300 pi-margin-bottom-30">
@@ -45,7 +45,7 @@
         <div class="pi-row">
 
             <!-- Col 4 -->
-            <div class="pi-col-xs-6 pi-col-sm-4 pi-padding-bottom-40">
+            <div class="pi-col-xs-6 pi-col-sm-4 pi-padding-bottom-10">
 
                 <div class="pi-icon-box pi-icon-box-hover pi-icon-box-icon-big">
 
@@ -200,7 +200,161 @@
     </div>
 </div>
 
+
+
+
+<!-- - - - - - - - - - PARA A EMPRESA SECTION - - - - - - - - - -->
+<div id="anchor-services" class="pi-section-w pi-section-dark">
+    <div class="pi-section pi-padding-top-30 pi-padding-bottom-20">
+
+        <div class="pi-text-center pi-padding-bottom-40">	
+            <h6 class="pi-uppercase pi-letter-spacing-3 pi-text-pink pi-margin-bottom-15">
+                Oferecemos.
+            </h6>
+            <h2 class="h1 pi-weight-700 pi-uppercase pi-has-border pi-has-tall-border pi-has-short-border">
+                Para sua Empresa!
+            </h2>
+        </div>
+
+        <!-- Row -->
+        <div class="pi-row">
+            <div class="pi-margin-bottom-15 pi-col-xs-12 pi-col-sm-7">
+                Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus autem
+Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo facilis est et expedita distinctio minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem cum soluta nobis est eligendi quibusdam et aut officiis debitis autem cum soluta nobis est aut optio cumque nihil.
+            </div>
+            <div class="pi-margin-bottom-15 pi-col-xs-12 pi-col-sm-5">
+                <img src="<?= Router::url('View/webroot/site/img/BALADA-6.jpg') ?>">
+            </div>
+        </div>
+    </div>
+</div>
+<!-- - - - - - - - - - END SECTION - - - - - - - - - -->
+
+
+<!-- - - - - - - - - - PARA A EMPRESA SECTION - - - - - - - - - -->
+<div id="anchor-services" class="pi-section-w pi-section-dark">
+    <div class="pi-section pi-padding-top-0 pi-padding-bottom-60">
+
+        <div class="pi-text-center pi-padding-bottom-40">	
+            <h2 class="h1 pi-weight-700 pi-uppercase pi-has-border pi-has-tall-border pi-has-short-border">
+                Para seu Cliente!
+            </h2>
+        </div>
+
+        <!-- Row -->
+        <div class="pi-row">
+            <div class="pi-margin-bottom-15 pi-col-xs-12 pi-col-sm-7">
+                Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus autem
+Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo facilis est et expedita distinctio minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem cum soluta nobis est eligendi quibusdam et aut officiis debitis autem cum soluta nobis est aut optio cumque nihil.
+            </div>
+            <div class="pi-margin-bottom-15 pi-col-xs-12 pi-col-sm-5">
+                <img src="<?= Router::url('View/webroot/site/img/shutterstock_107906300.jpg') ?>">
+            </div>
+        </div>
+    </div>
+</div>
 <!-- - - - - - - - - - END SECTION - - - - - - - - - -->
 
 
 
+<!-- - - - - - - - - - CONTACT SECTION - - - - - - - - - -->
+<div id="anchor-services" class="pi-section-w pi-section-white">
+    <div class="pi-section pi-padding-top-30 pi-padding-bottom-60">
+        <div class="pi-section">
+
+            <div class="pi-row">
+                <div class="pi-col-sm-6">
+                    <h2 class="h4 pi-has-bg pi-weight-700 pi-uppercase pi-letter-spacing pi-margin-bottom-30">
+                        ENTRE EM CONTATO
+                    </h2>
+
+                    <!-- Contact form -->
+                    <form role="form" action="handlers/formContact.php" class="pi-contact-form">
+
+                        <div class="pi-error-container"></div>
+
+                        <div class="form-group">
+                            <label for="exampleInputName1">Nome *</label>
+                            <input type="text" class="form-control form-control-name" id="exampleInputName1" name="nome"
+                                   placeholder="ex. Adam Smith">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">E-mail *</label>
+                            <input type="email" class="form-control form-control-email" id="exampleInputEmail1" name="email"
+                                   placeholder="ex. mail@example.com">
+                        </div>
+
+
+                        <div class="form-group">
+                            <label for="exampleInputCompany">Empresa que trabalha</label>
+                            <input type="text" class="form-control form-control-company-name" id="exampleInputCompany" name="empresa"
+                                   placeholder="ex. PI Studio">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="exampleInputPhone">Telefone *</label>
+                            <input type="text" class="form-control form-control-phone" id="exampleInputPhone" name="telefone" placeholder="ex. 55 41.99999999">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="exampleInputSelect">Assunto de envio</label>
+                            <input type="text" class="form-control form-control-phone" id="exampleInputSelect" name="assunto" placeholder="ex. Solicitação de cadastro">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="exampleInputMessage1">Mensagem *</label>
+                            <textarea class="form-control form-control-comments" id="exampleInputMessage1" placeholder="Qual sua mensagem para nós?"
+                                      rows="3"></textarea>
+                        </div>
+                        <p>
+                            <button class="btn pi-btn pi-btn-base">
+                                Enviar e-mail
+                            </button>
+                        </p>
+                    </form>
+                    <!-- End contact form -->
+
+                </div>
+                <div class="pi-col-sm-6">
+
+
+
+                    <h2 class="h4 pi-has-bg pi-weight-700 pi-uppercase pi-letter-spacing pi-margin-bottom-30">
+                        NOS CONTATE.
+                    </h2>
+
+                    <p>
+                        Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est
+                        eligendi.
+                    </p>
+
+                    <p class="h6 pi-uppercase pi-weight-700">
+                        <i class="icon-mail pi-text-base" style="margin-right: 5px;"></i> <a href="#" class="pi-link-dark">CONTATO@MYNIGHT.COM.BR</a>
+                    </p>
+
+                    <p class="h6 pi-uppercase pi-weight-700  pi-padding-bottom-30">
+                        <i class="icon-phone pi-text-base" style="margin-right: 5px;"></i> +1 (330) 993-443
+                    </p>
+
+                    <p>
+                        Monday - Friday: <strong>9:00 am - 10:00 pm</strong>
+                        <br>
+                        Saturday - Sunday: <strong>Closed</strong>
+                    </p>
+
+                    <ul class="pi-social-icons pi-jump pi-colored-bg pi-small pi-round pi-padding-top-10  pi-padding-bottom-30">
+                        <li><a href="#" class="pi-social-icon-facebook"><i class="icon-facebook"></i></a></li>
+                        <li><a href="#" class="pi-social-icon-twitter"><i class="icon-twitter"></i></a></li>
+                        <li><a href="#" class="pi-social-icon-dribbble"><i class="icon-dribbble"></i></a></li>
+                        <li><a href="#" class="pi-social-icon-rss"><i class="icon-rss"></i></a></li>
+                    </ul>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<!-- - - - - - - - - - END SECTION - - - - - - - - - -->
