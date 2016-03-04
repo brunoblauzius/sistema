022b0f3cfb815c869eb9935f79f061b8359a3245
Bruno Blauzius Schuindt <bruno_newstudio@hotmail.com>
2016-03-04 17:11:36 -0300

    email_contato

--- ServidorDeEmails/View/Layouts/html/email_cadastro_cmcpf.php
+++
@@ -1,5 +0,0 @@
-<strong>
-    Nome: <?= $nome;?><br>
-    E-mail: <?= $email;?><br>
-    Telefone: <?= $ddd;?>. <?= $telefone;?><br>
-</strong>
---
+++ ServidorDeEmails/View/Layouts/html/email_contato.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Teste</title>
+    </head>
+    <body style="font-family: Arial; font-size:14px;">
+        
+        <table style="margin:auto; width:600px;">
+            <thead style="background-color:#303030; color:#f7f7f7;">
+                <tr>
+                    <td  style="padding: 10px;">
+                        <img src="logo-base.png" style="margin-right:30px; height:70px; vertical-align: middle"> <h3 style="color:#FFF; font-size:26px; float: right"> Solicitação de Reserva </h3>
+                    </td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td style="padding: 20px;">
+                        
+                        <h4 style="color:#fc9448">Olá Bruno Blauzius,</h4>
+                        <p style="text-align: justify; font-size:13px;">
+                            Segue abaixo as informações da sua reserva, para <strong>TAJ BAR</strong> no dia <strong>01/03/2016</strong> às <strong>19:00</strong> hrs.
+                        </p>
+                        
+                        <div style="background-color:#f9f9f9; padding:10px; font-size:13px;">
+                            <table style="width:100%;" cellpadding="3">
+                                <tbody>
+                                    <tr>
+                                        <td colspan="2"><h2 style="color:#fc9448; text-align: center">Dados da reserva.</h2></td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width:30%">Número de convidados:</td>
+                                        <td><strong>10</strong></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Local da reserva:</td>
+                                        <td><strong>VARANDA SHED 1</strong></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Mesas:</td>
+                                        <td><strong>mesa 1</strong></td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="2" style="text-align: center; height: 80px;">
+                                            <a href="#" style="background-color:#fc9448; text-decoration: none; color:#fff; padding:10px; border-radius:5px; ">
+                                                Confirmar presença
+                                            </a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                                <tfoot>
+                                    <tr>
+                                        <td colspan="2">
+                                            <div style="font-size:12px; margin-top:20px;">
+                                                Atenciosamente,<br>
+                                                Alcieli/ Patricia<br>
+                                                Departamento reservas<br>
+                                                TAJ BAR /SANTA MARTA/ SHED<br>
+                                                3014-4467/ 3079-6151<br>
+                                                www.tajbar.com.br; www.santamartabar.com.br; www.shedbar.com.br <br>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                        
+                    </td>
+                </tr>
+            </tbody>
+            <tfoot>
+                <tr style="background-color:#303030; color:#f7f7f7;">
+                    <td style="padding: 10px; text-align:center" >
+                        <img src="logo-base.png" style="margin-right:15px; height:50px; vertical-align: middle"> © 2015. <span style="color:#fc9448">Sistema de reservas My Night</span>. Todos os direitos reservados.
+                    </td>
+                </tr>
+            </tfoot>
+        </table>
+        
+        
+    </body>
+</html>
